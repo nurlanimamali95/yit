257cc2aa7d49bb639e4e58c70e759028469a7d77
Nurlan <nurlanimamali@gmail.com>
2024-12-16 17:37:06 +0100

    initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#FFC544",
+				secondary: "#B3E6FF",
+				accent: "#B2FFB2",
+				dark: "#555555",
+				white: "#FFFFFF",
+			},
+			backgroundImage: {
+				"custom-gradient":
+					"linear-gradient(90deg, #FFFFFF 20%, #FFE5AF 54%, #B5E2FF 65%)",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
