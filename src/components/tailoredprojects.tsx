341a788da5f8ab19c867e9a4f0e63f616ffce2e9
Nurlan <nurlanimamali@gmail.com>
2024-12-17 20:00:15 +0100

    home page is done

---
+++ src/components/tailoredprojects.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+export default function TailoredProjects() {
+	const [hoveredCard, setHoveredCard] = useState<"first" | "second" | null>(
+		null
+	);
+
+	const gradients = {
+		first: "linear-gradient(to right, #FFCA5F 0%, #B5E2FF 49%)",
+		second: "linear-gradient(to right, #B5E2FF 32%, #FFCA5F 100%)",
+	};
+
+	return (
+		<section className="mx-[3rem] md:mx-[5rem] my-[3rem] md:my-[10rem]">
+			<div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 items-stretch">
+				<motion.div
+					onHoverStart={() => setHoveredCard("first")}
+					onHoverEnd={() => setHoveredCard(null)}
+					animate={{
+						background:
+							hoveredCard === "second" ? gradients.second : gradients.first,
+					}}
+					transition={{
+						background: { duration: 0.7, ease: "easeInOut" },
+					}}
+					className="p-6 md:p-8 rounded-xl shadow-lg h-[300px] md:h-[350px] lg:h-[400px] flex flex-col justify-between"
+				>
+					<h3 className="text-xl font-bold mb-4">For Schools:</h3>
+					<p className="leading-relaxed md:text-xl lg:text-2xl">
+						All of my theater projects can be tailor-made to suit your needs! I
+						will focus on every child’s interests and preferences to ensure that
+						each of them has an unforgettable performance arts journey.
+					</p>
+				</motion.div>
+
+				<motion.div
+					onHoverStart={() => setHoveredCard("second")}
+					onHoverEnd={() => setHoveredCard(null)}
+					animate={{
+						background:
+							hoveredCard === "first" ? gradients.first : gradients.second,
+					}}
+					transition={{
+						background: { duration: 0.7, ease: "easeInOut" },
+					}}
+					className="p-6 md:p-8 rounded-xl shadow-lg h-[300px] md:h-[350px] lg:h-[400px] flex flex-col justify-between"
+				>
+					<h3 className="text-xl font-bold mb-4">
+						For individuals and families:
+					</h3>
+					<p className="md:text-xl lg:text-2xl leading-relaxed">
+						If you are 6 to 16 years old, welcome to my theater world where art,
+						drama, culture, technology, and nature converge to create
+						performances and inspire wonder.
+					</p>
+				</motion.div>
+			</div>
+		</section>
+	);
+}
